30dbede7d29c715dfab88e7face43240e1036dff
skyline <skylinety@icloud.com>
2023-01-07 16:48:25 +0800

    feat: 雪碧图动画

---
+++ src/pages/animations/animatedPic/index.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+import './index.scss'
+
+const WorkingPic: FC = () => (
+  <div className='WorkingPic'>
+    <div id='sprite-container'>
+      <div
+        id='sprite-image'
+      ></div>
+    </div>
+  </div>
+)
+
+export default WorkingPic
